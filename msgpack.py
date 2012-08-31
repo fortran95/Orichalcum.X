346927f8477d5529e709b15f6d190351e9eb2221
lucifer <fortran95@126.com>
2012-08-31 21:50:48 +0800

    fix GMT

--- msgpack.py
+++ msgpack.py
@@ -1,6 +1,9 @@
 import json,zlib,time
 def enpack(tag,message,usexi):
-    return json.dumps({'t':tag,'m':zlib.compress(message,9).encode('base64').replace('\n',''),'x':usexi,'i':time.time()})
+    return json.dumps({'t':tag,
+                       'm':zlib.compress(message,9).encode('base64').replace('\n',''),
+                       'x':usexi,
+                       'i':time.time() + time.timezone})
 
 def depack(s):
     x = json.loads(s)
