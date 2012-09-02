c8b5a9049e9fd65776f7ab520840a3b55d04ab6e
lucifer <fortran95@126.com>
2012-09-02 23:22:55 +0800

    special settings in entity cache

--- entity.py
+++ entity.py
@@ -6,7 +6,7 @@ import ConfigParser
 import utils
 
 BASEPATH = os.path.realpath(os.path.dirname(sys.argv[0]))
-entitycache  = os.path.join(BASEPATH,'configs','entity.cache')
+entitycache  = os.path.join(BASEPATH,'cache','entity.cache')
 entityconfig = os.path.join(BASEPATH,'configs','alias.cfg')
 
 if os.path.isfile(entitycache):
@@ -14,7 +14,10 @@ if os.path.isfile(entitycache):
 else:
     cachetime = 0
 
-if cachetime <= os.path.getmtime(entityconfig):
+if cachetime <= max(os.path.getmtime(entityconfig),
+                    os.path.getmtime(os.path.realpath(sys.argv[0]))):
+    print 'Generating entity cache.'
+
     sh = shelve.open(entitycache,writeback=True)
     sh.clear()
 
@@ -22,10 +25,20 @@ if cachetime <= os.path.getmtime(entityconfig):
     cfg.read(entityconfig)
     
     for nickname in cfg.sections():
-        sh[nickname] = []
-        for x,jid in cfg.items(nickname):
-            sh[nickname].append(utils.stripJID(jid))
-
+        sh[nickname] = {'special':{}, 'jids':[]}
+        for x,y in cfg.items(nickname):
+            x = x.strip().lower()
+            if x.startswith('account'):
+                sh[nickname]['jids'].append(utils.stripJID(y))
+            elif x == 'allow_special':
+                specials = [t.strip().lower() for t in y.split(',')]
+                for special in specials:
+                    if not special in ('alert','warning'):
+                        continue
+                    if special.startswith('*'):
+                        sh[nickname]['special'][special[1:]] = True
+                    else:
+                        sh[nickname]['special'][special] = False
     sh.close()
 
 def getNicknameByJID(jid):
@@ -33,7 +46,7 @@ def getNicknameByJID(jid):
     sh = shelve.open(entitycache)
     jid = utils.stripJID(jid)
     for nickname in sh:
-        if jid in sh[nickname]:
+        if jid in sh[nickname]['jids']:
             sh.close()
             return nickname
     sh.close()
@@ -43,5 +56,5 @@ def getJIDsByNickname(nickname):
     global entitycache
     sh = shelve.open(entitycache)
     if sh.has_key(nickname):
-        return sh[nickname]
+        return sh[nickname]['jids']
     return False
